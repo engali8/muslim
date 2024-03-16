3f658cb5951787d0e7f4671eb93ad3febbe66b2d
engali8 <https://github.com/engali8/ninjawy.git>
2024-03-16 03:37:42 +0200

    commit on test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:muslim/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( Muslim());
+    await tester.pumpWidget(const Muslim());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
